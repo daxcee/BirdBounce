3cd414cd068bc4539fd77debefbc2b959a945216
Linda He <bananaphone@JimHe-2.local>
2013-11-20 16:09:02 -0800

    added startmenu2.png

---
+++ Projectfiles/Bird.h
@@ -0,0 +1,16 @@
+//
+//  Bird.h
+//  Bounce
+//
+//  Created by Linda He on 11/20/13.
+//
+//
+
+#import "CCSprite.h"
+
+@interface Bird : CCSprite
+
+@end
+
+
+
---
+++ Projectfiles/Bird.m
@@ -0,0 +1,31 @@
+//
+//  Bird.m
+//  Bounce
+//
+//  Created by Linda He on 11/20/13.
+//
+//
+
+#import "Bird.h"
+
+@implementation Bird
+/*{    //determines whether the bird is falling
+    BOOL isFalling;
+    //changing velocity of the bird as it falls
+    int fallingSpeed;
+}
+
+-(id) initWithPosition:(NSPoint) position
+{
+    if ((self = [super init]))
+    {
+        isFalling = TRUE;
+        fallingSpeed = 0;
+        CCSprite *sprite = [CCSprite spriteWithFile:@"upbird.png"];
+        [self addChild:sprite z:2];
+    }
+    
+    return self;
+}*/
+
+@end
