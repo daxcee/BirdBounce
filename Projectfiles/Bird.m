08df254463e9403108da4f8b4376211a6ff7089b
Linda He <bananaphone@JimHe-2.local>
2013-11-20 16:44:21 -0800

    Attempting to create and use a Bird class

--- Projectfiles/Bird.m
+++ Projectfiles/Bird.m
@@ -7,25 +7,31 @@
 //
 
 #import "Bird.h"
+#import "Bounce.h"
 
 @implementation Bird
-/*{    //determines whether the bird is falling
+{    //determines whether the bird is falling
     BOOL isFalling;
     //changing velocity of the bird as it falls
     int fallingSpeed;
 }
 
--(id) initWithPosition:(NSPoint) position
+-(id) initWithPosition:(int) pathXPos
 {
     if ((self = [super init]))
     {
         isFalling = TRUE;
         fallingSpeed = 0;
-        CCSprite *sprite = [CCSprite spriteWithFile:@"upbird.png"];
-        [self addChild:sprite z:2];
+        CCSprite *bird = [CCSprite spriteWithFile:@"upbird.png"];
+        bird.position = ccp(pathXPos, 480);
     }
-    
     return self;
-}*/
+}
+
+-(id) init
+{
+    //by default creates a bird in the middle
+    return [self initWithPosition: 320/2];
+}
 
 @end
--- Projectfiles/Bounce.m
+++ Projectfiles/Bounce.m
@@ -1,10 +1,17 @@
 #import "Bounce.h"
+#import "Bird.h"
 
 
-CCSprite *ball1;
-CCSprite *box1;
-CCSprite *box2;
-CCSprite *box3;
+
+#define TRAMPOLINEYPOS 420/8
+#define PATH1 320/7
+#define PATH2 320/2
+#define PATH3 320/7*6
+//CCSprite *ball1;
+CCSprite *bird1;
+CCSprite *bird2;
+CCSprite *bird3;
+
 bool box1Touched = false;
 
 @interface Bounce (PrivateMethods)
@@ -20,25 +27,25 @@ bool box1Touched = false;
         sprite.anchorPoint = CGPointZero;
         [self addChild:sprite z:-1];
         
-        // Add ball 1 to screen
+        /*// Add ball 1 to screen
         ball1 = [CCSprite spriteWithFile:@"upbird.png"];
         ball1.position = ccp(320/7, 420);
-        [self addChild:ball1 z:2];
+        [self addChild:ball1 z:2];*/
         
-        // Initialize box 1
-        box1 = [CCSprite spriteWithFile:@"redbouncer.png"];
-        box1.position = ccp(320/7, 420/8);
-        [self addChild:box1 z:1];
+        // Initialize red bird (left)
+        bird1 = [CCSprite spriteWithFile:@"redbouncer.png"];
+        bird1.position = ccp(TRAMPOLINEYPOS, PATH1);
+        [self addChild:bird1 z:1];
         
-        // Initialize box 2
-        box2 = [CCSprite spriteWithFile:@"greenbouncer.png"];
-        box2.position = ccp(320/2, 420/8);
-        [self addChild:box2 z:1];
+        // Initialize green bird (center)
+        bird2 = [CCSprite spriteWithFile:@"greenbouncer.png"];
+        bird2.position = ccp(TRAMPOLINEYPOS, PATH2);
+        [self addChild:bird2 z:1];
         
-        // Initialize box 3
-        box3 = [CCSprite spriteWithFile:@"bluebouncer.png"];
-        box3.position = ccp(320/7*6, 420/8);
-        [self addChild:box3 z:1];
+        // Initialize blue bird (center)
+        bird3 = [CCSprite spriteWithFile:@"bluebouncer.png"];
+        bird3.position = ccp(TRAMPOLINEYPOS, PATH3);
+        [self addChild:bird3 z:1];
         
         [self scheduleUpdate];
 
@@ -49,6 +56,8 @@ bool box1Touched = false;
 
 - (void) update:(ccTime)dt
 {
+    Bird *firstBird = [[Bird alloc] initWithPosition PATH1];
+    
     
 //    //move the ship only in the x direction by a fixed amount every frame
 //    ship1.position = ccp( ship1.position.x + 100*dt, ship1.position.y );
@@ -61,7 +70,7 @@ bool box1Touched = false;
 //
     
 
-    if (!box1Touched) {
+/*    if (!box1Touched) {
         ball1.position = ccp(ball1.position.x, ball1.position.y - 200*dt);
     } else if (box1Touched) {
         ball1.position = ccp(ball1.position.x, ball1.position.y + 200*dt);
@@ -76,14 +85,14 @@ bool box1Touched = false;
         //get x and y coordinates for mouse
         int x = pos.x;
         int y = pos.y;
-        CGFloat distance = abs(ball1.position.y-box1.position.y); // get distance between ball 1 and box 1
+        CGFloat distance = abs(ball1.position.y-bird1.position.y); // get distance between ball 1 and box 1
         
-        CGFloat maxDistance = (ball1.contentSize.height + box1.contentSize.height)/2; // minimum distance in order to bounce back up
+        CGFloat maxDistance = (ball1.contentSize.height + bird1.contentSize.height)/2; // minimum distance in order to bounce back up
         
-        CGFloat x1 = (box1.position.x-(box1.contentSize.width/2));
-        CGFloat x2 = (box1.position.x+(box1.contentSize.width/2));
-        CGFloat y1 = (box1.position.y-(box1.contentSize.height/2));
-        CGFloat y2 = (box1.position.y+(box1.contentSize.height/2));
+        CGFloat x1 = (bird1.position.x-(bird1.contentSize.width/2));
+        CGFloat x2 = (bird1.position.x+(bird1.contentSize.width/2));
+        CGFloat y1 = (bird1.position.y-(bird1.contentSize.height/2));
+        CGFloat y2 = (bird1.position.y+(bird1.contentSize.height/2));
         
         if (x >= x1 && x <= x2 && y >= y1 && y <= y2 && distance <= maxDistance) {
             box1Touched = true;
@@ -91,6 +100,8 @@ bool box1Touched = false;
         
         CCLOG(@"Does it work? I think it did!");
     }
+    */
+    
     
 }
 
