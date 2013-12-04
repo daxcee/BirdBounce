4eb5d539abbb2c1013dad0aab318794aa523ba12
tbehailu <tsionbehailu@gmail.com>
2013-12-04 14:58:15 -0800

    added Score changes

---
+++ Score.h
@@ -0,0 +1,13 @@
+//
+//  Score.h
+//  Bounce
+//
+//  Created by Tsion Behailu on 12/4/13.
+//
+//
+
+#import "CCSprite.h"
+
+@interface Score : CCSprite
+
+@end
---
+++ Score.m
@@ -0,0 +1,13 @@
+//
+//  Score.m
+//  Bounce
+//
+//  Created by Tsion Behailu on 12/4/13.
+//
+//
+
+#import "Score.h"
+
+@implementation Score
+
+@end
